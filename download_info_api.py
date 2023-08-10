db432c3353bf59db0f8b40b546d1e7e3df13275b
Arturomo <81843008+Arturomo@users.noreply.github.com>
2023-08-10 11:00:34 -0600

    Update download_info_api.py

--- download_info_api.py
+++ download_info_api.py
@@ -2,33 +2,6 @@ import requests, psycopg2, os
 import pandas as pd
 from datetime import datetime
 
-def create_tables():
-    """
-    Crea la tabla en la base de datos en caso de que no exista
-    """
-    create_table = """
-            CREATE TABLE IF NOT EXISTS arturomontesdeoca4892_coderhouse.clima_mexico
-            (id_estado VARCHAR(5) NOT NULL PRIMARY KEY,
-            nombre VARCHAR(20) NOT NULL,
-            temperatura NUMERIC(15,2) NOT NULL,
-            uv_index NUMERIC(15,2) NOT NULL,
-            prob_precipitacion NUMERIC(15,2) NOT null,
-            humedad NUMERIC(15,2) not null,
-            velocidad_viento_Kmph NUMERIC(15,2) not null,
-            direccion_viento VARCHAR(5) not null,
-            descripcion VARCHAR(100) not null,
-            zona_cercana VARCHAR(60) not null,
-            iluminacion_lunar NUMERIC(15,2) not null,
-            fase_lunar VARCHAR(30) not null,
-            info_date timestamp not null);
-            """
-    conn = psycopg2.connect(os.getenv["redshift_string_connection"])
-    cur = conn.cursor()
-    cur.execute(create_table)
-    conn.commit()
-    conn.close()
-
-
 def weather_info(city):
     """
     Extrae la informacion de los datos de la ciudad establcida
@@ -137,5 +110,4 @@ def main():
 
 
 if __name__ == '__main__':
-    create_tables()
     main()
